7e7a054d1dfb00644464225fcba4f8c2cf5aa8ae
layhome12 <layhome12@gmail.com>
2023-01-28 18:50:32 +0700

    Express Initialize

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Testing");
+import express, { Application, Request, Response } from "express";
+
+class App {
+  public app: Application;
+  public static port: number = 80;
+
+  constructor() {
+    this.app = express();
+    this.routes();
+  }
+
+  protected routes(): void {
+    this.app.route("/").get((req: Request, res: Response) => {
+      res.send("Okee");
+    });
+  }
+}
+
+const app = new App().app;
+app.listen(App.port, () => {
+  console.log("Server running at http://localhost:" + App.port);
+});
